7233b9ba4668729af5b683df909b05d1c84ab078
AhmadZaki8332 <ahmadzaki8332@gmail.com>
2024-10-26 20:28:25 +0700

    Update topbar dan main activity

--- app/src/main/java/com/example/aquasmart10/MainActivity.kt
+++ app/src/main/java/com/example/aquasmart10/MainActivity.kt
@@ -9,29 +9,43 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.aquasmart10.component.MainTopBar
 import com.example.aquasmart10.ui.theme.AquaSmart10Theme
 
-@Composable
-fun MainTopBar(modifier: Modifier = Modifier) {
-    Column {
-        Row {
-            Image(
-                painter = painterResource(R.drawable.img_background),
-                contentDescription = "Background"
-            )
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent(){
+            AquaSmart10Theme {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AquaSmart()
+                }
+            }
         }
     }
 }
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    AquaSmart10Theme {
+fun AquaSmart(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+    ) {
         MainTopBar()
     }
 }
--- app/src/main/java/com/example/aquasmart10/component/MainTopBar.kt
+++ app/src/main/java/com/example/aquasmart10/component/MainTopBar.kt
@@ -2,44 +2,88 @@ package com.example.aquasmart10.component
 
 import com.example.aquasmart10.R
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.material3.Icon
-import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+
+val poppinsFamily = FontFamily(
+        Font(R.font.poppins_regular, FontWeight.Normal),
+        Font(R.font.poppins_bold, FontWeight.Bold)
+
+)
 
 @Composable
 fun MainTopBar(modifier: Modifier = Modifier) {
-        Column (
+
+        Column(
                 modifier = Modifier
-                        .fillMaxWidth(),
-                horizontalAlignment = Alignment.
-        ){
-                Row {
+                        .fillMaxWidth()
+                        .background(color = Color(0xFF5E7BF9))
+                        .padding(20.dp)
+        ) {
+                Row(
+                        modifier = Modifier
+                                .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                ) {
                         Image(
                                 painter = painterResource(R.drawable.img_1),
-                                contentDescription = null
+                                contentDescription = "logo AquaSmart",
+                                modifier = Modifier
+                                        .size(70.dp)
                         )
-
                         Icon(
-                                modifier = Modifier,
+                                modifier = Modifier
+                                        .padding(top = 15.dp)
+                                        .size(32.dp),
                                 imageVector = Icons.Default.Notifications,
+                                tint = Color.White,
                                 contentDescription = "Notifikasi"
-
                         )
                 }
+                Text(
+                        text = "Selamat Datang,",
+                        fontSize = 24.sp,
+                        color = Color.White,
+                        fontFamily = poppinsFamily,
+
+                        modifier = Modifier
+                                .padding(top = 10.dp),
+                )
+                Text(
+                        text = "Budi",
+                        fontSize = 32.sp,
+                        color = Color.White,
+                        fontFamily = poppinsFamily,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                                .padding(top = 5.dp),
+                )
+
+
         }
 }
 
 @Preview
 @Composable
-fun MainTopBarPreview() {
+private fun MainTopBarPreview() {
         MainTopBar()
 }
